6d4942fc9b9be99085bf0b4f0349bfcfb46aeaa6
AlenaBel <46319442+AlenaBel@users.noreply.github.com>
2020-07-05 15:54:11 +0300

    Add files via upload

--- Task_1.py
+++ Task_1.py
@@ -3,8 +3,29 @@ name = input('Enter your name: ')
 email = input('Enter your email: ')
 phone = input('Enter your phone number: ')
 github = input('Enter link to your Github profile: ')
-save = input('Save to CSV? ')
-if save == 'yes':
- file = open('results.csv', 'a')
- csv_writer = csv.writer(file)
- csv_writer.writerow([name, github, email, phone])
+job = input('Are you looking for a new job?')
+if job == 'yes':
+    np = input('What is your notice period?(asap/x days/weeks/months)')
+    if np == 'asap':
+        print('Dear %s, we will contact you within 1 business day' % name)
+        save = input('Save to CSV? ')
+        if save == 'yes':
+          file = open('results.csv', 'a')
+          csv_writer = csv.writer(file)
+          csv_writer.writerow([name, github, email, phone, job, np])
+        pass
+    else:
+        save = input('Save to CSV? ')
+        if save == 'yes':
+          file = open('results.csv', 'a')
+          csv_writer = csv.writer(file)
+          csv_writer.writerow([name, github, email, phone, job, np])
+          pass
+    pass
+else:
+    save = input('Save to CSV?')
+    if save == 'yes':
+        file = open('results.csv', 'a')
+        csv_writer = csv.writer(file)
+        csv_writer.writerow([name, github, email, phone, job])
+    pass
